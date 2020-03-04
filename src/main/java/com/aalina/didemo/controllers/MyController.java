3c75761e9e8f41607ad1d50b5a9fb074de26cac0
Alina <alinaarap@gmail.com>
2020-03-04 18:22:31 +0200

    Primary Bean

--- src/main/java/com/aalina/didemo/controllers/MyController.java
+++ src/main/java/com/aalina/didemo/controllers/MyController.java
@@ -1,9 +1,15 @@
 package com.aalina.didemo.controllers;
 
+import com.aalina.didemo.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+    private GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
 
     public String hello() {
         System.out.println("Hello!");
